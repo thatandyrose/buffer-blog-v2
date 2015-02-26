5a4753d57ba49bff8a787a2697767abc26a0e54e
Andy <thatandyrose@gmail.com>
2015-02-26 22:35:03 +0000

    redid and replaced logo

--- wp-content/themes/buffer-v2/header.php
+++ wp-content/themes/buffer-v2/header.php
@@ -50,7 +50,9 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home <?php bloginfo( 'name' ); ?>" class="site-title"><span class="logotype"></span><?php bloginfo( 'name' ); ?></a>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home <?php bloginfo( 'name' ); ?>" class="site-title">
+				<?php bloginfo( 'name' ); ?>
+			</a>
 			<div class="tagline"><?php bloginfo( 'description' ); ?></div>
 		</div>
 
