84c0dd8fa915eaf987305574d2b862909c0d6049
Andy <thatandyrose@gmail.com>
2015-02-26 21:45:30 +0000

    removed hardcoded sharing

--- wp-content/themes/buffer-v2/content.php
+++ wp-content/themes/buffer-v2/content.php
@@ -25,18 +25,6 @@
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php buffer_posted_on(); ?>
-
-			<div class="share-buttons">
-
-				<a href="https://twitter.com/share" class="twitter-share-button" data-via="buffer">Tweet</a>
-				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-
-				<a href="http://bufferapp.com/add" class="buffer-add-button" data-count="horizontal" data-via="buffer" ></a>
-				<script type="text/javascript" src="https://d389zggrogs7qo.cloudfront.net/js/button.js"></script>
-
-			</div>
-
-
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
